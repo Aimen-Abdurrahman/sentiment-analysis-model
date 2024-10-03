08382c8691bafb7f7735a795266214d9f9402ae3
Aimen Abdurrahman <aimen@live.de>
2024-10-03 19:12:07 +0300

    init: base file for training the model

---
+++ train_model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+
+print(device)
+
+
